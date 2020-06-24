8154575380a4f27eb628e8af2d05e737b736b6dc
Lee1729 <wlwl2003@naver.com>
2020-06-24 14:41:53 +0900

    final

--- LAP/views.py
+++ LAP/views.py
@@ -5,30 +5,10 @@ from os.path import isfile
 import requests
 
 from django.shortcuts import render, redirect
-
 from tensorflow.keras import backend as K
-from django.conf import settings
-from django.views.generic import ListView
-from django.views.generic import TemplateView
-from django.views.generic.edit import CreateView
-from rest_framework import views
-from rest_framework import status
-from rest_framework.generics import get_object_or_404
-from rest_framework.parsers import FormParser
-from rest_framework.parsers import MultiPartParser
-from rest_framework.response import Response
-from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.decorators import api_view
 
-from .forms import FileForm
-from LAP.models import FileModel
-from LAP.serializers import FileSerializer
-
-from music21 import midi
-
 from models.MusicGenerator import MusicGenerator
-
-from django.core.files import File
 from django.http import FileResponse
 
 def index(request):
@@ -43,7 +23,7 @@ def endPage(request):
     filename = 'example'
     music_gen = MusicGenerator()
     score = music_gen.Generate()
-    #music_gen.notes_to_midi('models/', score, filename)
+    music_gen.notes_to_midi('models/', score, filename)
     music_gen.notes_to_png('models/', score, filename)
 
     return render(request, 'endPage.html')
